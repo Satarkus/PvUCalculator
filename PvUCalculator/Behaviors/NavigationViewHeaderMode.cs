6b31d22501612dc21d2afdcec79697e164f93375
Satarkus <sbrianstark@gmail.com>
2021-09-09 12:14:08 +0200

    Add project files.

---
+++ PvUCalculator/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace PvUCalculator.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ PvUCalculator/Views/AboutPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using PvUCalculator.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace PvUCalculator.Views
+{
+    public sealed partial class AboutPage : Page
+    {
+        public AboutViewModel ViewModel { get; } = new AboutViewModel();
+
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
